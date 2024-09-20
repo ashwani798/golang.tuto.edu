f1e09575d54326055e7a945d7d94040a31374448
Ashwani kumar pandey <aahwanikumarpandey@gmail.com>
2024-09-20 19:25:55 +0530

    Array

---
+++ 07myarray/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("welcome to array in golang")
+
+	var FruitList [4]string
+
+	FruitList[0] = "Apple"
+	FruitList[1] = "Orange"
+	FruitList[3] = "Peach"
+
+	fmt.Println("Fruit list is: ", FruitList)      //Fruit list is:  [Apple Orange  Peach]
+	fmt.Println("Fruit list is: ", len(FruitList)) //Fruit list is:  4
+
+	var vegList = [5]string{"potato", "tomato", "onion"}
+	fmt.Println("veg list is:", len(vegList))
+}
